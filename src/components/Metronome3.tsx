8c815f004acf834ffe68c377035309d56f1dbcb5
kongbhumi <kopmul26@gmail.com>
2024-10-02 23:38:28 +0700

    fix audio 4

--- src/components/Metronome3.tsx
+++ src/components/Metronome3.tsx
@@ -47,9 +47,9 @@ export const Metronome3 = () => {
 
   const handleTap = () => {
     if (!audioCtx) return;
-    alert(audioCtx?.audioContext?.state)
-    if(audioCtx?.audioContext?.state == 'suspended'){
-      
+    //alert(audioCtx?.audioContext?.state)
+    if(audioCtx?.audioContext?.state == 'interrupted'){
+      alert('bro')
       //audioCtx?.audioContext?.resume();
     }
     
