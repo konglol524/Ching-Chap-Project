f8584faff3495498ed14dc15aa59ce5463bbae3b
kongbhumi <kopmul26@gmail.com>
2024-10-03 13:00:27 +0700

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { DropdownNavigator } from "@/components/Dropdown";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <DropdownNavigator></DropdownNavigator>
         {children}
+        <Analytics />
         </body>
     </html>
   );
