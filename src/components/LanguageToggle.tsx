75d2aac1bf120ebdfe7d94d07143ea0d5d1217a0
kongbhumi <kopmul26@gmail.com>
2024-10-05 22:15:37 +0700

    minor changes to button text

--- src/components/LanguageToggle.tsx
+++ src/components/LanguageToggle.tsx
@@ -27,7 +27,7 @@ export const LanguageToggle = () => {
     onClick={toggleLanguage}
     className="ml-4 px-4 py-2  bg-gray-800 text-white font-medium text-sm rounded-md hover:bg-gray-700 hover:text-red-400 focus:outline-none focus:ring-2 focus:ring-red-500 transition-all duration-300"
   >
-      {i18n.language === "en" ? "ภาษาไทย" : "English"}
+      {i18n.language === "en" ? "TH" : "EN"}
     </button>
   );
 };
